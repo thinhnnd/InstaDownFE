97bfdb9e6bee930d8ca97c30500392d01d8a633c
thinhnnd <16521179@gm.uit.edu.vn>
2020-04-22 12:29:42 +0700

    add single image page

--- src/views/pages/SingleImagePage.js
+++ src/views/pages/SingleImagePage.js
@@ -1,19 +1,66 @@
 import React from "react";
-import ExamplesNavbar from 'components/Navbars/ExamplesNavbar';
-import IndexNavbar from 'components/Navbars/IndexNavbar';
 import MainFooter from "components/Footers/MainFooter.js";
-import LandingPageHeader from 'components/Headers/LandingPageHeader';
+import DefaultNavbar from "components/Navbars/DefaultNavbar";
+import Img from 'react-image';
 
-function SingleImagePage () {
-   
+import {
+    Container,
+    Row,
+    Button,
+    Col
+} from "reactstrap";
+
+import { useParams } from "react-router-dom"
+
+function SingleImagePage(props) {
+    let  link = props.match.params.link; 
+    console.log(link);
+
     return (
-        <div style={ { backgroundColor: '#fefefe' }}>
-            <IndexNavbar />
-           
-            <h3>Hello</h3>
+        <div className="instadown" style={{ backgroundColor: '#fefefe' }}>
+            <div className="section section-media mt-5">
+                <DefaultNavbar />
+
+                <Container>
+                    <Row>
+                        <Col md="9">
+                            <div className="text-center">
+                                <div class="img-wrapper">
+                                    {/* <img src="https://cdn.pixabay.com/photo/2012/10/25/23/52/wolf-62898_960_720.jpg" /> */}
+                                    <img src="https://cdn.pixabay.com/photo/2016/03/09/09/39/shoes-1245920_1280.jpg" />
+                                </div>
+                            </div>
+
+                        </Col>
+                        <Col md="3" className="side-function">
+                            <button class="btn btn-primary btn-fullwith">
+                                <i class="fa fa-download"></i> DownLoad Free
+                            </button>
+                            <div>
+                                <button class="btn btn-secondary btn-fullwith">
+                                    <i class="fa fa-pencil-square"></i> Add WaterMark
+                                </button>
+                            </div>
+                            <div className="share text-center">
+                                <button class="btn btn-danger btn-just-icon">
+                                    <i class="fa fa-heart"></i>
+                                </button>
+                                <button class="btn btn-info btn-just-icon">
+                                    <i class="fa fa-facebook"></i>
+                                </button>
+                                <button class="btn btn-success btn-just-icon">
+                                    <i class="fa fa-link"></i>
+                                </button>
+                            </div>
+
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
             <MainFooter />
+
         </div>
-      );
+    );
 }
- 
+
 export default SingleImagePage;
