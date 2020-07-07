3cbf981c550b77ecb8852b1794956abad98c0a60
thinhnnd <16521179@gm.uit.edu.vn>
2020-07-07 18:09:09 +0700

    fix server error

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
-app.listen('Server is listening on port: ' + 9000);
+app.listen(port);
